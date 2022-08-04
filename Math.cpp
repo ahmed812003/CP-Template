4de6a560fd4fd6d472b799cb23ecd12cc813f2cd
ahmed yaser <ay2309133@gmail.com>
2022-08-04 14:09:46 +0200

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -20,6 +20,11 @@ ll lcm(ll a, ll b) {
     return a / gcd(a, b) * b;
 }
 
+bool is_perfect_square(ll n){
+    ll sq = sqrt(n);
+    return sq * sq == n;
+}
+
 bool prime_check(ll x){
     if(x<=1)
         return false;
@@ -61,6 +66,16 @@ void prime_factors(ll n) {
     }
 }
 
+vector < int > prime_factorization(ll n){
+    vector < int > factors;
+    while(n % 2 == 0) factors.push_back(2), n /= 2;
+    for(int i = 3; i <= sqrt(n); i += 2)
+        while(n % i == 0) factors.push_back(i), n /= i;
+    if(n > 2) factors.push_back(n);
+    return factors;
+}
+
+
 const ll x=10000001;
 bool prime[x+1];
 void sieve (){
@@ -96,6 +111,26 @@ for(int i=2 ; i<=m ; i++){
         for(int j=i;j<=N;j+=i)
             take[j] = 1;
     }
+}
+
+void Print_Permutation(vector < int >& nums){
+    sort(all(nums));
+    do {
+        for(auto& i : nums)
+            cout << i << " ";
+        cout << "\n";
+    } while(next_permutation(nums.begin(), nums.end()));
+}
+
+void Print_Permutation(string s){
+    sort(all(s));
+    do {
+        cout << s << "\n";
+    } while(next_permutation(s.begin(), s.end()));
+}
+
+bool is_triangle(ll a, ll b, ll c){
+    return (a + b > c) && (a + c > b) && (b + c > a) && (a && b && c);
 }
 
 int main()

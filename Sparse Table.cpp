895cc3e1338b089f5b2415090cfb492c2556edb9
ahmed yaser <ay2309133@gmail.com>
2022-11-01 16:50:14 +0200

    Update Sparse Table.cpp

--- Sparse Table.cpp
+++ Sparse Table.cpp
@@ -11,33 +11,30 @@ void fast()
 }
 
 const int N = 2e5 + 7 , bits = 27;
-ll table [bits][N] , arr[N];
+ll table [bits][N] , arr[N] , LOG[N];;
 
 ll merge (ll x, ll y) {
-    return min(x , y);
+    return max(x , y);
 }
 
-ll query(int l , int r) {
-    int length = r - l + 1;
-    ll ret = INT_MAX;
-    for (int i = 0 ; i <= bits ; i++) {
-        if (length & (1 << i)) {
-            ret = merge(
-                      ret,
-                      table[i][l]
-                  );
-            l += (1 << i);
-        }
-    }
-    return ret;
+ll query(int l, int r) {
+    if (l == r)return 0;
+    if (r < l)swap(l, r);
+    int msk = LOG[r - l + 1];
+    return max(
+               table[msk][l],
+               table[msk][r - (1 << msk) + 1]
+           );
+
 }
 
 void bulit (int size) {
-    for (int i = 0 ; i < size ; i++) {
+    for (int i = 1 ; i <= size ; i++) {
         table[0][i] = arr[i];
+        LOG[i + 1] = LOG[(i + 1) >> 1] + 1;
     }
     for (int mask = 1 ; mask <= bits ; mask++) {
-        for (int i = 0 ; i + (1 << mask) <= size ; i++) {
+        for (int i = 1 ; i + (1 << mask) - 1 <= size ; i++) {
             table[mask][i] = merge(
                                  table[mask - 1][i],
                                  table[mask - 1][i + (1 << (mask - 1))]
